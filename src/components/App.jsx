678758e40b297accac7ca505638bed552485ea3a
OlehScherbak <103534824+OlehScherbak@users.noreply.github.com>
2023-02-06 14:29:46 +0200

    add componentDidMount

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,14 @@ import ContactList from './ContactList/ContactList';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+    this.setState({ contacts: savedContacts });
+  }
 
   componentDidUpdate(prevProps, prevState) {
     prevState.contacts !== this.state.contacts &&
