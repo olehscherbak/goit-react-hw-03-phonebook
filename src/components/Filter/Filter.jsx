484fbd2f5fb1d83f25d183a8e35a40e27e633318
Oleh.Scherbak <oleh.scherbak@ukr.net>
2023-02-11 11:08:49 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,16 +2,15 @@ import PropTypes from 'prop-types';
 import css from './Filter.module.css';
 
 export default function Filter({ onChange }) {
-  const filterValuePass = e => {
-    onChange(e.target.value);
-  };
   return (
     <input
       type="text"
       name="filter"
       autoComplete="off"
       className={css.filter}
-      onChange={filterValuePass}
+      onChange={e => {
+        onChange(e.target.value);
+      }}
     />
   );
 }
